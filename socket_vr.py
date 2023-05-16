4fe3bfc55a3ceeb3d1116ff4e3f0eafee41257eb
Dongeun <tjehddms123@gmail.com>
2023-05-16 09:25:04 +0900

    Update socket_vr.py

--- socket_vr.py
+++ socket_vr.py
@@ -26,7 +26,7 @@ PROJECT_ID = "coopgo-b58bd"
 cred = credentials.Certificate("/home/ksb/coopgo-b58bd-firebase-adminsdk-q4m0h-913cfe20db.json")
 default_app = firebase_admin.initialize_app(cred,{'storageBucket':f"{PROJECT_ID}.appspot.com"})
 db = firestore.client()
-bucket = storage.bucket()#기본 버킷 사용
+bucket = storage.bucket()
 print("firebase setup end")
 
 #capture
@@ -44,6 +44,7 @@ def savePhoto(frame,filename,img_path):
     cv2.imwrite(img_path, frame)
 
 def runModel(img_path, objects) : 
+    objects = []
     print("Run Model")
     results = model(img_path)
     detections = results.pandas().xyxy[0]
@@ -54,41 +55,23 @@ def runModel(img_path, objects) :
             objects[cls_name] = 0
         objects[cls_name]+= 1
         
-    if(objects != {}) :
+    if objects :                          #if any object is detected
         results.show()
         #db.collection(u'detectionResult').document(u'5znMimhJJau6OYtILKM4').set(results)
         #results.save(labels=True,save_dir= img_path)
         #uploadPhoto(results)
         #db.collection(u'detectionResult').document(str_now).set(objects)
         print("DB upload")
-        
-    #if(results):
-        #results.show()
-        #db.collection(u'detectionResult').document(u'5znMimhJJau6OYtILKM4').set(results)
-        #
 
+#upload photo on firebase storage        
 def uploadPhoto(file):
-    #파베 스토리지 주소: /pictures/ + file
     blob = bucket.blob('pictures/' + file)
-    #blob.make_public()
-    # new token, metadata 설정
     new_token = uuid4()
     metadata = {"firebaseStorageDownloadTokens": new_token}  # access token 필요
     blob.metadata = metadata
     blob.upload_from_filename(filename=picture_directory+file)
-    print("사진 delete & 업로드 완료")
+    print("Photo delete on local space & Uploade Complete")
     print(blob.public_url)
-
-#flask
-@app.route('/api/qr-detection',methods=['POST'])
-def handle_post_request():
-    if request.method=='POST':
-        data = request.json
-        print(data)
-        response_data="Received Post request"
-        return response_data,200
-if __name__=='__main__':
-    app.run(host='192.168.137.114',port=8000)  
 
 #do capture 
 while True: 
@@ -104,29 +87,37 @@ while True:
                 if data:
                     print(data.decode())
                     key = data['id']
-                    # 응답할 데이터
-                    response_data = "Received POST request"
-                    # 클라이언트에게 응답
+                   
+                    response_data = "Received POST request"   # Response
                     response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}".format(len(response_data), response_data)
                     conn.sendall(response.encode())
-                    capture_cnt = 5
-                    while capture_cnt > 0:
+                
+                    capture_cnt = 5       # number of times capturing objs
+                    while capture_cnt :
+                        capture_cnt-= 1 
+                       
                         now = datetime.datetime.now()
                         str_now = str(now.strftime('%y%m%d%H%M%S'))
-                        filename = str_now + '.jpg'
-                        ret, img= cap.read()
+                        filename = str_now + '.jpg'   # name filename
+                        
+                        ret, img= cap.read()      #read img
                         img_path = ""
+                        
+                        #save & upload photo
                         savePhoto(img, filename, img_path)
-                        uploadPhoto(filename)        
+                        uploadPhoto(filename)
+                        
+                        #find one optimal detection out of 5 times of capturing
                         objects = {}
                         max_obj_num = 0
                         max_objects = {}
-                        runModel(img_path, objects)
-                        if(objects.size() > max_obj_num):
+                        runModel(img_path, objects)     # run yolov5 coop-go model
+                        
+                        if(objects.size() > max_obj_num):    #update max_obj_num & max_objects
                             max_obj_num = objects.size()
                             max_objects = objects
+                            
+                        #send data 
                         conn.sendall(max_objects)
+                        #upload collection on firestore
                         db.collection(u'detectionResult').document(key).set(max_objects)
-                        capture_cnt-= 1
-                        
- 
